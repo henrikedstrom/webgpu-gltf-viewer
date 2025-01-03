007b5655cdf6b29a052a59e1e23e6706e6c1a47e
henrikedstrom <henrik.c.edstrom@gmail.com>
2025-01-03 15:39:00 -0800

    Refactor Renderer class. Remove local functions. Make Adapter and Device setup more robust.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -10,7 +10,6 @@
 
 // Third-Party Library Headers
 #include <GLFW/glfw3.h>
-#include <webgpu/webgpu_cpp.h>
 
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_FORCE_RIGHT_HANDED
@@ -32,107 +31,157 @@
 #include "renderer.h"
 
 //----------------------------------------------------------------------
-// Internal Utility Functions and Variables - [TODO] All to be refactored
+// Renderer Class implementation
 
-namespace
+void Renderer::Initialize(GLFWwindow *window, Camera *camera, Model *model, uint32_t width, uint32_t height,
+                          const std::function<void()> &callback)
 {
+    m_window = window;
+    m_camera = camera;
+    m_model = model;
+    m_width = width;
+    m_height = height;
 
-wgpu::Instance instance;
-wgpu::Adapter adapter;
-wgpu::Device device;
-wgpu::RenderPipeline pipeline;
-wgpu::Buffer vertexBuffer;
-wgpu::Buffer indexBuffer;
-wgpu::Buffer uniformBuffer;
-wgpu::BindGroup bindGroup;
-wgpu::Surface surface;
-wgpu::TextureFormat format;
-wgpu::Texture depthTexture;
-wgpu::TextureView depthTextureView;
+    m_instance = wgpu::CreateInstance();
+    GetAdapter([this, callback](wgpu::Adapter adapter) {
+        m_adapter = adapter;
+        GetDevice([this, callback](wgpu::Device device) {
+            m_device = device;
 
-struct Uniforms
+#if defined(__EMSCRIPTEN__)
+            wgpu::SurfaceDescriptorFromCanvasHTMLSelector canvasDesc{};
+            canvasDesc.selector = "#canvas";
+            wgpu::SurfaceDescriptor surfaceDesc{.nextInChain = &canvasDesc};
+            m_surface = m_instance.CreateSurface(&surfaceDesc);
+#else
+            m_surface = wgpu::glfw::CreateSurfaceForWindow(m_instance, m_window);
+#endif
+
+            InitGraphics();
+
+            // Return control to the application
+            callback();
+        });
+    });
+}
+
+void Renderer::Render()
 {
-    glm::mat4 viewMatrix;
-    glm::mat4 projectionMatrix;
-    glm::mat4 modelMatrix;
-};
+    UpdateUniforms();
 
-Uniforms uniforms;
-
-//----------------------------------------------------------------------
-// Utility functions
-
-std::string LoadShaderFile(const std::string &filepath)
-{
-    std::ifstream file(filepath, std::ios::in | std::ios::binary);
-    if (!file.is_open())
+    wgpu::SurfaceTexture surfaceTexture;
+    m_surface.GetCurrentTexture(&surfaceTexture);
+    if (!surfaceTexture.texture)
     {
-        std::cerr << "Failed to open shader file: " + filepath + "\n";
-        return "";
+        std::cerr << "Error: Failed to get current surface texture." << std::endl;
+        return;
     }
 
-    std::ostringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
+    wgpu::RenderPassColorAttachment attachment{.view = surfaceTexture.texture.CreateView(),
+                                               .loadOp = wgpu::LoadOp::Clear,
+                                               .storeOp = wgpu::StoreOp::Store,
+                                               .clearValue = {.r = 0.0f, .g = 0.2f, .b = 0.4f, .a = 1.0f}};
+
+    wgpu::RenderPassDepthStencilAttachment depthAttachment{
+        .view = m_depthTextureView,
+        .depthLoadOp = wgpu::LoadOp::Clear,
+        .depthStoreOp = wgpu::StoreOp::Store,
+        .depthClearValue = 1.0f, // Clear to the farthest value
+        .stencilLoadOp = wgpu::LoadOp::Clear,
+        .stencilStoreOp = wgpu::StoreOp::Store,
+    };
+
+    wgpu::RenderPassDescriptor renderpass{
+        .colorAttachmentCount = 1, .colorAttachments = &attachment, .depthStencilAttachment = &depthAttachment};
+
+    wgpu::CommandEncoder encoder = m_device.CreateCommandEncoder();
+    wgpu::RenderPassEncoder pass = encoder.BeginRenderPass(&renderpass);
+    pass.SetPipeline(m_pipeline);
+    pass.SetBindGroup(0, m_bindGroup);
+    pass.SetVertexBuffer(0, m_vertexBuffer);
+    pass.SetIndexBuffer(m_indexBuffer, wgpu::IndexFormat::Uint32);
+    pass.DrawIndexed(static_cast<uint32_t>(m_model->GetIndices().size()));
+    pass.End();
+
+    wgpu::CommandBuffer commands = encoder.Finish();
+    m_device.GetQueue().Submit(1, &commands);
+
+#if !defined(__EMSCRIPTEN__)
+    m_surface.Present();
+    m_instance.ProcessEvents();
+#endif
 }
 
-//----------------------------------------------------------------------
+void Renderer::InitGraphics()
+{
+    ConfigureSurface();
+    CreateVertexBuffer();
+    CreateIndexBuffer();
+    CreateUniformBuffer();
+    CreateDepthTexture();
+    CreateRenderPipeline();
+}
 
-void ConfigureSurface(uint32_t width, uint32_t height)
+void Renderer::ConfigureSurface()
 {
     wgpu::SurfaceCapabilities capabilities;
-    surface.GetCapabilities(adapter, &capabilities);
-    format = capabilities.formats[0];
-
-    wgpu::SurfaceConfiguration config{.device = device, .format = format, .width = width, .height = height};
-    surface.Configure(&config);
+    m_surface.GetCapabilities(m_adapter, &capabilities);
+    m_surfaceFormat = capabilities.formats[0];
+    wgpu::SurfaceConfiguration config{
+        .device = m_device, .format = m_surfaceFormat, .width = m_width, .height = m_height};
+    m_surface.Configure(&config);
 }
 
-void CreateVertexBuffer(const std::vector<float> &vertexData)
+void Renderer::CreateVertexBuffer()
 {
+    const std::vector<float> vertexData = m_model->GetVertices();
+
     wgpu::BufferDescriptor vertexBufferDesc{};
     vertexBufferDesc.size = vertexData.size() * sizeof(float);
     vertexBufferDesc.usage = wgpu::BufferUsage::Vertex | wgpu::BufferUsage::CopyDst;
     vertexBufferDesc.mappedAtCreation = true;
 
-    vertexBuffer = device.CreateBuffer(&vertexBufferDesc);
-    std::memcpy(vertexBuffer.GetMappedRange(), vertexData.data(), vertexData.size() * sizeof(float));
-    vertexBuffer.Unmap();
+    m_vertexBuffer = m_device.CreateBuffer(&vertexBufferDesc);
+    std::memcpy(m_vertexBuffer.GetMappedRange(), vertexData.data(), vertexData.size() * sizeof(float));
+    m_vertexBuffer.Unmap();
 }
 
-void CreateIndexBuffer(const std::vector<uint32_t> &indexData)
+void Renderer::CreateIndexBuffer()
 {
+    const std::vector<uint32_t> indexData = m_model->GetIndices();
+
     wgpu::BufferDescriptor indexBufferDesc{};
     indexBufferDesc.size = indexData.size() * sizeof(uint32_t);
     indexBufferDesc.usage = wgpu::BufferUsage::Index | wgpu::BufferUsage::CopyDst;
     indexBufferDesc.mappedAtCreation = true;
 
-    indexBuffer = device.CreateBuffer(&indexBufferDesc);
-    std::memcpy(indexBuffer.GetMappedRange(), indexData.data(), indexData.size() * sizeof(uint32_t));
-    indexBuffer.Unmap();
+    m_indexBuffer = m_device.CreateBuffer(&indexBufferDesc);
+    std::memcpy(m_indexBuffer.GetMappedRange(), indexData.data(), indexData.size() * sizeof(uint32_t));
+    m_indexBuffer.Unmap();
 }
 
-void CreateUniformBuffer()
+void Renderer::CreateUniformBuffer()
 {
     wgpu::BufferDescriptor bufferDescriptor{};
     bufferDescriptor.size = sizeof(Uniforms);
     bufferDescriptor.usage = wgpu::BufferUsage::Uniform | wgpu::BufferUsage::CopyDst;
 
-    uniformBuffer = device.CreateBuffer(&bufferDescriptor);
+    m_uniformBuffer = m_device.CreateBuffer(&bufferDescriptor);
 
     // Initialize the uniforms with default values
+    Uniforms uniforms;
     uniforms.viewMatrix = glm::mat4(1.0f);       // Identity matrix
     uniforms.projectionMatrix = glm::mat4(1.0f); // Identity matrix
     uniforms.modelMatrix = glm::mat4(1.0f);      // Identity matrix
 
-    device.GetQueue().WriteBuffer(uniformBuffer, 0, &uniforms, sizeof(Uniforms));
+    m_device.GetQueue().WriteBuffer(m_uniformBuffer, 0, &uniforms, sizeof(Uniforms));
 }
 
-void CreateBindGroup(wgpu::BindGroupLayout bindGroupLayout)
+void Renderer::CreateBindGroup(wgpu::BindGroupLayout bindGroupLayout)
 {
     wgpu::BindGroupEntry bindGroupEntry{};
     bindGroupEntry.binding = 0;
-    bindGroupEntry.buffer = uniformBuffer;
+    bindGroupEntry.buffer = m_uniformBuffer;
     bindGroupEntry.offset = 0;
     bindGroupEntry.size = sizeof(Uniforms);
 
@@ -141,28 +190,28 @@ void CreateBindGroup(wgpu::BindGroupLayout bindGroupLayout)
     bindGroupDescriptor.entryCount = 1;
     bindGroupDescriptor.entries = &bindGroupEntry;
 
-    bindGroup = device.CreateBindGroup(&bindGroupDescriptor);
+    m_bindGroup = m_device.CreateBindGroup(&bindGroupDescriptor);
 }
 
-void CreateDepthTexture(uint32_t width, uint32_t height)
+void Renderer::CreateDepthTexture()
 {
     wgpu::TextureDescriptor depthTextureDescriptor{};
-    depthTextureDescriptor.size = {width, height, 1};
+    depthTextureDescriptor.size = {m_width, m_height, 1};
     depthTextureDescriptor.format = wgpu::TextureFormat::Depth24PlusStencil8;
     depthTextureDescriptor.usage = wgpu::TextureUsage::RenderAttachment;
 
-    depthTexture = device.CreateTexture(&depthTextureDescriptor);
-    depthTextureView = depthTexture.CreateView();
+    m_depthTexture = m_device.CreateTexture(&depthTextureDescriptor);
+    m_depthTextureView = m_depthTexture.CreateView();
 }
 
-void CreateRenderPipeline()
+void Renderer::CreateRenderPipeline()
 {
     wgpu::ShaderModuleWGSLDescriptor wgslDesc{};
     const std::string shader = LoadShaderFile("./assets/shaders/basic.wgsl");
     wgslDesc.code = shader.c_str();
 
     wgpu::ShaderModuleDescriptor shaderModuleDescriptor{.nextInChain = &wgslDesc};
-    wgpu::ShaderModule shaderModule = device.CreateShaderModule(&shaderModuleDescriptor);
+    wgpu::ShaderModule shaderModule = m_device.CreateShaderModule(&shaderModuleDescriptor);
 
     wgpu::VertexAttribute vertexAttributes[] = {
         {.format = wgpu::VertexFormat::Float32x3, .offset = 0, .shaderLocation = 0},
@@ -174,7 +223,7 @@ void CreateRenderPipeline()
                                                 .attributeCount = 3,
                                                 .attributes = vertexAttributes};
 
-    wgpu::ColorTargetState colorTargetState{.format = format};
+    wgpu::ColorTargetState colorTargetState{.format = m_surfaceFormat};
 
     wgpu::FragmentState fragmentState{
         .module = shaderModule, .entryPoint = "fragmentMain", .targetCount = 1, .targets = &colorTargetState};
@@ -199,7 +248,7 @@ void CreateRenderPipeline()
         .entries = &bindGroupLayoutEntry,
     };
 
-    wgpu::BindGroupLayout bindGroupLayout = device.CreateBindGroupLayout(&bindGroupLayoutDescriptor);
+    wgpu::BindGroupLayout bindGroupLayout = m_device.CreateBindGroupLayout(&bindGroupLayoutDescriptor);
 
     // Step 2: Create the bind group
     CreateBindGroup(bindGroupLayout);
@@ -210,7 +259,7 @@ void CreateRenderPipeline()
         .bindGroupLayouts = &bindGroupLayout,
     };
 
-    wgpu::PipelineLayout pipelineLayout = device.CreatePipelineLayout(&layoutDescriptor);
+    wgpu::PipelineLayout pipelineLayout = m_device.CreatePipelineLayout(&layoutDescriptor);
 
     wgpu::RenderPipelineDescriptor descriptor{.layout = pipelineLayout,
                                               .vertex =
@@ -224,32 +273,26 @@ void CreateRenderPipeline()
                                               .depthStencil = &depthStencilState,
                                               .fragment = &fragmentState};
 
-    pipeline = device.CreateRenderPipeline(&descriptor);
+    m_pipeline = m_device.CreateRenderPipeline(&descriptor);
 }
 
-void UpdateUniforms(const Camera &camera, const Model &model)
+void Renderer::UpdateUniforms() const
 {
-    uniforms.modelMatrix = model.GetTransform();
-    uniforms.viewMatrix = camera.GetViewMatrix();
-    uniforms.projectionMatrix = camera.GetProjectionMatrix();
+    Uniforms uniforms;
+    uniforms.viewMatrix = m_camera->GetViewMatrix();
+    uniforms.projectionMatrix = m_camera->GetProjectionMatrix();
+    uniforms.modelMatrix = m_model->GetTransform();
 
-    device.GetQueue().WriteBuffer(uniformBuffer, 0, &uniforms, sizeof(Uniforms));
+    m_device.GetQueue().WriteBuffer(m_uniformBuffer, 0, &uniforms, sizeof(Uniforms));
 }
 
-void InitGraphics(uint32_t width, uint32_t height, Model *model)
+void Renderer::GetAdapter(const std::function<void(wgpu::Adapter)> &callback)
 {
-    ConfigureSurface(width, height);
-    CreateVertexBuffer(model->GetVertices());
-    CreateIndexBuffer(model->GetIndices());
-    CreateUniformBuffer();
-    CreateDepthTexture(width, height);
-    CreateRenderPipeline();
-}
+    wgpu::RequestAdapterOptions options{.compatibleSurface = m_surface,
+                                        .powerPreference = wgpu::PowerPreference::HighPerformance};
 
-void GetAdapter(const std::function<void(wgpu::Adapter)> &callback)
-{
-    instance.RequestAdapter(
-        nullptr,
+    m_instance.RequestAdapter(
+        &options,
         [](WGPURequestAdapterStatus status, WGPUAdapter cAdapter, const char *message, void *userdata) {
             if (message)
             {
@@ -268,10 +311,59 @@ void GetAdapter(const std::function<void(wgpu::Adapter)> &callback)
         new std::function<void(wgpu::Adapter)>(callback));
 }
 
-void GetDevice(const std::function<void(wgpu::Device)> &callback)
+void Renderer::GetDevice(const std::function<void(wgpu::Device)> &callback)
 {
-    adapter.RequestDevice(
-        nullptr,
+    wgpu::DeviceDescriptor deviceDesc;
+    deviceDesc.SetDeviceLostCallback(
+        wgpu::CallbackMode::AllowSpontaneous,
+        [](const wgpu::Device &device, wgpu::DeviceLostReason reason, const char *message) {
+            std::cerr << "Device lost: ";
+
+            // Print the reason
+            switch (reason)
+            {
+            case wgpu::DeviceLostReason::Unknown:
+                std::cerr << "[Reason: Unknown]";
+                break;
+            case wgpu::DeviceLostReason::Destroyed:
+                std::cerr << "[Reason: Destroyed]";
+                break;
+            case wgpu::DeviceLostReason::InstanceDropped:
+                std::cerr << "[Reason: Instance Dropped]";
+                break;
+            case wgpu::DeviceLostReason::FailedCreation:
+                std::cerr << "[Reason: Failed Creation]";
+                break;
+            default:
+                std::cerr << "[Reason: Unrecognized]";
+                break;
+            }
+
+            // Print the message
+            if (message)
+            {
+                std::cerr << " - " << message << std::endl;
+            }
+            else
+            {
+                std::cerr << " - No message provided." << std::endl;
+            }
+        });
+
+    deviceDesc.SetUncapturedErrorCallback([](const wgpu::Device &device, wgpu::ErrorType type, const char *message) {
+        std::cerr << "Uncaptured error: ";
+        if (message)
+        {
+            std::cerr << message << std::endl;
+        }
+        else
+        {
+            std::cerr << "No message provided." << std::endl;
+        }
+    });
+
+    m_adapter.RequestDevice(
+        &deviceDesc,
         [](WGPURequestDeviceStatus status, WGPUDevice cDevice, const char *message, void *userdata) {
             if (message)
             {
@@ -290,82 +382,16 @@ void GetDevice(const std::function<void(wgpu::Device)> &callback)
         new std::function<void(wgpu::Device)>(callback));
 }
 
-} // namespace
-
-//----------------------------------------------------------------------
-// Renderer Class implementation
-
-void Renderer::Initialize(GLFWwindow *window, Camera *camera, Model *model, uint32_t width, uint32_t height,
-                          const std::function<void()> &callback)
+std::string Renderer::LoadShaderFile(const std::string &filepath) const
 {
-    m_window = window;
-    m_camera = camera;
-    m_model = model;
-    m_width = width;
-    m_height = height;
+    std::ifstream file(filepath, std::ios::in | std::ios::binary);
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open shader file: " + filepath + "\n";
+        return "";
+    }
 
-    instance = wgpu::CreateInstance();
-    GetAdapter([window, width, height, model, callback](wgpu::Adapter a) {
-        adapter = a;
-        GetDevice([window, width, height, model, callback](wgpu::Device d) {
-            device = d;
-
-#if defined(__EMSCRIPTEN__)
-            wgpu::SurfaceDescriptorFromCanvasHTMLSelector canvasDesc{};
-            canvasDesc.selector = "#canvas";
-
-            wgpu::SurfaceDescriptor surfaceDesc{.nextInChain = &canvasDesc};
-            surface = instance.CreateSurface(&surfaceDesc);
-#else
-            surface = wgpu::glfw::CreateSurfaceForWindow(instance, window);
-#endif
-
-            InitGraphics(width, height, model);
-
-            // Invoke the callback
-            callback();
-        });
-    });
-}
-
-void Renderer::Render() const
-{
-    // Update camera and model transformations
-    UpdateUniforms(*m_camera, *m_model);
-
-    wgpu::SurfaceTexture surfaceTexture;
-    surface.GetCurrentTexture(&surfaceTexture);
-
-    wgpu::RenderPassColorAttachment attachment{.view = surfaceTexture.texture.CreateView(),
-                                               .loadOp = wgpu::LoadOp::Clear,
-                                               .storeOp = wgpu::StoreOp::Store,
-                                               .clearValue = {.r = 0.0f, .g = 0.2f, .b = 0.4f, .a = 1.0f}};
-
-    wgpu::RenderPassDepthStencilAttachment depthAttachment{
-        .view = depthTextureView,
-        .depthLoadOp = wgpu::LoadOp::Clear,
-        .depthStoreOp = wgpu::StoreOp::Store,
-        .depthClearValue = 1.0f, // Clear to the farthest value
-        .stencilLoadOp = wgpu::LoadOp::Clear,
-        .stencilStoreOp = wgpu::StoreOp::Store,
-    };
-
-    wgpu::RenderPassDescriptor renderpass{
-        .colorAttachmentCount = 1, .colorAttachments = &attachment, .depthStencilAttachment = &depthAttachment};
-
-    wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
-    wgpu::RenderPassEncoder pass = encoder.BeginRenderPass(&renderpass);
-    pass.SetPipeline(pipeline);
-    pass.SetBindGroup(0, bindGroup);
-    pass.SetVertexBuffer(0, vertexBuffer);
-    pass.SetIndexBuffer(indexBuffer, wgpu::IndexFormat::Uint32);
-    pass.DrawIndexed(static_cast<uint32_t>(m_model->GetIndices().size()));
-    pass.End();
-    wgpu::CommandBuffer commands = encoder.Finish();
-    device.GetQueue().Submit(1, &commands);
-
-#if !defined(__EMSCRIPTEN__)
-    surface.Present();
-    instance.ProcessEvents();
-#endif
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
 }
--- src/renderer.h
+++ src/renderer.h
@@ -3,13 +3,17 @@
 // Standard Library Headers
 #include <cstdint>
 #include <functional>
+#include <string>
+#include <vector>
 
 // Third-Party Library Headers
-#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <webgpu/webgpu_cpp.h>
 
 // Forward Declarations
 class Camera;
 class Model;
+struct GLFWwindow;
 
 // Renderer Class
 class Renderer
@@ -19,21 +23,59 @@ class Renderer
     Renderer() = default;
 
     // Rule of 5
-    Renderer(const Renderer &) = default;
-    Renderer &operator=(const Renderer &) = default;
-    Renderer(Renderer &&) = default;
-    Renderer &operator=(Renderer &&) = default;
+    Renderer(const Renderer &) = delete;
+    Renderer &operator=(const Renderer &) = delete;
+    Renderer(Renderer &&) = delete;
+    Renderer &operator=(Renderer &&) = delete;
 
     // Public Interface
     void Initialize(GLFWwindow *window, Camera *camera, Model *model, uint32_t width, uint32_t height,
                     const std::function<void()> &callback);
-    void Render() const;
+    void Render();
 
   private:
-    // Private Member Variables
+    // Types
+    struct Uniforms
+    {
+        glm::mat4 viewMatrix;
+        glm::mat4 projectionMatrix;
+        glm::mat4 modelMatrix;
+    };
+
+    // Private member variables
+    GLFWwindow *m_window = nullptr;
     uint32_t m_width = 0;
     uint32_t m_height = 0;
-    GLFWwindow *m_window = nullptr;
     Camera *m_camera = nullptr;
     Model *m_model = nullptr;
+
+    // WebGPU variables
+    wgpu::Instance m_instance;
+    wgpu::Adapter m_adapter;
+    wgpu::Device m_device;
+    wgpu::Surface m_surface;
+    wgpu::TextureFormat m_surfaceFormat;
+    wgpu::RenderPipeline m_pipeline;
+    wgpu::Buffer m_vertexBuffer;
+    wgpu::Buffer m_indexBuffer;
+    wgpu::Buffer m_uniformBuffer;
+    wgpu::BindGroup m_bindGroup;
+    wgpu::Texture m_depthTexture;
+    wgpu::TextureView m_depthTextureView;
+
+    // Private utility methods
+    void InitGraphics();
+    void ConfigureSurface();
+    void CreateVertexBuffer();
+    void CreateIndexBuffer();
+    void CreateUniformBuffer();
+    void CreateBindGroup(wgpu::BindGroupLayout bindGroupLayout);
+    void CreateDepthTexture();
+    void CreateRenderPipeline();
+    void UpdateUniforms() const;
+    void GetAdapter(const std::function<void(wgpu::Adapter)> &callback);
+    void GetDevice(const std::function<void(wgpu::Device)> &callback);
+
+    // Shader utility
+    std::string LoadShaderFile(const std::string &filepath) const;
 };
